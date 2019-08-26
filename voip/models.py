5a4e22f5a603df0c17eecebc537c55ec3064c2b3
Ammar.Joher <Ammar.Joher@fintelligentsoftware.com>
2019-08-26 15:36:31 +0400

    Models changed to include foreign key.

--- voip/models.py
+++ voip/models.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models import ForeignKey
+
+
 class voip_user(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
     received_count = models.IntegerField(default=0)
     notes = models.TextField()
+    user = ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name;
